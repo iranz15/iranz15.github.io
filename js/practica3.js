90c24f03831587e73999c66f7f75e1436f6dae61
Jorge Iranzo <jorirsan@upv.es>
2022-09-29 04:12:58 +0200

    Practica 2/3

--- js/practica3.js
+++ js/practica3.js
@@ -3,11 +3,13 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'https://unpkg.com/three@0.140.1/examples/jsm/controls/OrbitControls.js';
 
+
 let angulo = 0;
-let scene,camera,renderer,robot;
+let scene,camera,renderer,robot,cameraHud;
 let cameraControls;
 let L=5;
 let planta,alzado,perfil;
+
 init();
 loadScene();
 render();
@@ -18,22 +20,103 @@ function init() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor(new THREE.Color(0xffffff));
     document.getElementById('container').appendChild(renderer.domElement);
-
-    scene = new THREE.Scene();
-
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(120, 250, 220);
-    camera.lookAt(new THREE.Vector3(0, 135, 0));
+    renderer.autoClear = false;
     
-    const cameraControls = new OrbitControls( camera,renderer.domElement );
-    cameraControls.target.set(0,135,0)
+    scene = new THREE.Scene();
+    // Si hay backgroudn no va
+    
+    const ar = window.innerWidth / window.innerHeight;
+    camera = new THREE.PerspectiveCamera(75, ar, 0.1, 1000);
+    //cameraHud = new THREE.OrthographicCamera(-2*ar,2*ar,2,-2,1,1000)
+    
+    setCameras(ar);
+    
+    camera.position.set(120, 250, 220);
+    camera.lookAt(new THREE.Vector3(0, 1, 0));
+    cameraControls = new OrbitControls( camera,renderer.domElement );
+    cameraControls.target.set(0,1,0)
+    
+    // Captura eventos
+    window.addEventListener('resize',updateAspectRatio);
+    //renderer.domElement.addEventListener('dblclick',rotateShape);
+
 }
 
+/*
+function rorateSHape(evento){
+    //Atentcuon al pickfing
+}
+*/
+function setCameras(ar){
+ 
+    let camaraOrtografica;
+    
+    if (ar > 1) {
+        camaraOrtografica = new THREE.OrthographicCamera( -L*ar, L*ar, L, -L, -10, 100);
+    }
+    else{
+        camaraOrtografica = new THREE.OrthographicCamera( -L, L, L/ar, -L/ar, -10, 100);  
+    }
+    
+    alzado = camaraOrtografica.clone();
+    alzado.position.set(0,0,L);
+    alzado.lookAt(0,0,0);
+    
+    //planta
+    planta = camaraOrtografica.clone();
+    planta.position.set(0,L,0);
+    planta.lookAt(0,0,0);
+    planta.up = new THREE.Vector3(0,0,-1)
+    
+    //perfil
+    perfil = camaraOrtografica.clone();
+    perfil.position.set(L,0,0);
+    perfil.lookAt(0,0,0);
+    //perfil.up = new THREE.Vector3(0,0,-1)
+}
+
+function updateAspectRatio(){
+    renderer.setSize(window.innerWidth,window.innerHeight)
+    const ar = window.innerWidth / window.innerHeight;
+    camera.aspect = ar;
+
+    
+    // Actualiza la vista de Orto
+    if (ar>1){
+        alzado.left = planta.left = perfil.left = -L*ar;
+        alzado.right = planta.right = perfil.right = L*ar;
+        alzado.top = planta.top = perfil.top = L;
+        alzado.bottom = planta.bottom = perfil.bottom = -L;
+        
+    }else{
+        alzado.left = planta.left = perfil.left = -L;
+        alzado.right = planta.right = perfil.right = L;
+        alzado.top = planta.top = perfil.top = L/ar;
+        alzado.bottom = planta.bottom = perfil.bottom = -L/ar;
+        
+    }
+    alzado.updateProjectionMatrix();
+    planta.updateProjectionMatrix();
+    perfil.updateProjectionMatrix();
+    camera.updateProjectionMatrix();
+    // cameraHud.left = -2 * ar
+    // cameraHud.right = 2 * ar
+    
+
+}
+
+function calculaNormales(normales){
+    for(let i = 0; i<normales.length();i++){
+        break
+    }
+}
+    
 function loadScene() {
 
     //Materiales
-    //onst matBase = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: false });
-    const matBase = new THREE.MeshNormalMaterial()
+    // const matBase = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: false });
+    const matBase = new THREE.MeshNormalMaterial();
+
     //Geometrias
     const geometriaSuelo = new THREE.PlaneGeometry(1000, 1000, 20, 20)
     const geometriaBase = new THREE.CylinderGeometry(50, 50, 15, 30);
@@ -53,8 +136,9 @@ function loadScene() {
     // Variables de ajuste de posicion
     const z = 10;
     const y = 10;
+    // *3
     const posicion = [
-        /*
+
         0,5-y,38-z, //0
         2,5-y,38-z, //1
         2,15-y,38-z, //2 
@@ -62,44 +146,7 @@ function loadScene() {
         2,20-y,19-z, //4
         0,20-y,19-z, //5
         0,0-y,19-z, //6
-        2,0-y,19-z, //7
-        */
-        //Delante
-        0,5-y,38-z, //0 -> 0 
-        2,5-y,38-z, //1 -> 1
-        2,15-y,38-z, //2 -> 2
-        0,15-y,38-z, //3 ->  3
-
-        //Arriba
-        0,15-y,38-z, //3 -> 4
-        2,15-y,38-z, //2 -> 5
-        2,20-y,19-z, //4 -> 6
-        0,20-y,19-z, //5 -> 7
-        
-        //atras
-        2,20-y,19-z, //4 -> 8
-        0,20-y,19-z, //5 -> 9
-        2,0-y,19-z, //7 -> 10
-        0,0-y,19-z, //6 -> 11
-
-        //abajo
-        2,5-y,38-z, //1 -> 12 
-        0,5-y,38-z, //0 -> 13
-        0,0-y,19-z, //6 -> 14
-        2,0-y,19-z, //7 ->  15
-
-
-        //derecha
-        2,5-y,38-z, //1 -> 16
-        2,0-y,19-z, //7 -> 17
-        2,20-y,19-z, //4 -> 18
-        2,15-y,38-z, //2 -> 19
-
-        //izquierda
-        0,5-y,38-z, //0 ->  20
-        0,15-y,38-z, //3 -> 21 
-        0,20-y,19-z, //5 -> 22
-        0,0-y,19-z //6 ->  23
+        2,0-y,19-z //7
 
     ]
   
@@ -109,45 +156,49 @@ function loadScene() {
         0,1,2,
         2,3,0,
         //arriba
+        3,2,4,
+        4,5,4,
+        //atras
         4,5,6,
         6,7,4,
-        //atras
-        8,10,11,
-        11,9,8,
         //abajo
-        12,13,14,
-        14,15,12,
+        0,6,7,
+        7,1,0,
         //derecha
-        //17,18,19,
-        //19,16,17,
-        16,17,18,
-        18,19,16,
+        2,4,7,
+        7,1,2,
         //izquierda
-        20,21,22,
-        22,23,20
+        //0,3,5,
+        //5,6,0
+        3,5,6,
+        6,0,3
 
     ]
-    // ^i(a2b3−a3b2)−^j(a1b3−a3b1)+^k(a1b2−a2b1)
-    var normales = [ // 24 x3
 
+    var normales = [ // 24 
     0,0,1, 0,0,1, 0,0,1, 0,0,1,      // Front
-    1,0,0, 1,0,0, 1,0,0, 1,0,0,      // Right
-    0,0,-1, 0,0,-1, 0,0,-1, 0,0,-1,  // Back 
-    -1,0,0, -1,0,0, -1,0,0, -1,0,0,  // Left
     0,1,0, 0,1,0, 0,1,0, 0,1,0,      // Top 
-    0,-1,0, 0,-1,0, 0,-1,0, 0,-1,0   // Bottom
+    0,0,-1, 0,0,-1, 0,0,-1, 0,0,-1,  // Back 
+    0,-1,0, 0,-1,0, 0,-1,0, 0,-1,0,   // Bottom
+    1,0,0, 1,0,0, 1,0,0, 1,0,0,      // Right
+    -1,0,0, -1,0,0, -1,0,0, -1,0,0  // Left
     ];
+    
 
     geometriaDedoPinza.setIndex( indices );
     geometriaDedoPinza.setAttribute( 'position', new THREE.Float32BufferAttribute(posicion,3));
-    const bufferPosition = geometriaDedoPinza.getAttribute('position');
-    for(let i = 0;i<posicion.length/3;i++){
-        const b = new THREE.Vector3( );
-        b.fromBufferAttribute(bufferPosition,i)
-    }
-
     geometriaDedoPinza.setAttribute( 'normal', new THREE.Float32BufferAttribute(normales,3));
-
+    var normal = geometriaDedoPinza.getAttribute('normal');
+    var position = geometriaDedoPinza.getAttribute('position');
+    
+    /*
+    var origin = new THREE.Vector3(position.array[0], position.array[1], position.array[2]);
+    var dir = new THREE.Vector3(normal.array[0], normal.array[1], normal.array[2])
+    var helper = new THREE.ArrowHelper(dir, origin, 1, 0x00ff00);
+    helper.position.copy(origin);
+    scene.add(helper);
+    */
+    
     //Meshes
     robot = new THREE.Object3D();
     const base = new THREE.Mesh(geometriaBase, matBase);
@@ -212,7 +263,7 @@ function loadScene() {
 
     pinzaD.position.set(0, 8, 6);
     pinzaI.position.set(0, -8, 6);
-    const suelo = new THREE.Mesh(geometriaSuelo, new THREE.MeshBasicMaterial({ color: 'red', wireframe: true }))
+    const suelo = new THREE.Mesh(geometriaSuelo, new THREE.MeshNormalMaterial())
     suelo.rotateX(-Math.PI / 2);
     
 
@@ -253,7 +304,28 @@ function loadScene() {
 function render() {
     requestAnimationFrame(render);
     update();
+    
+    //Borar una unica vez
+    renderer.clear();
+    // Repartir el canvas en cuatro viewports con la misma relacion de aspecto
+    // Origen es bottom left
+    
+    //Abajo Izquiera
+    renderer.setViewport(0,0,window.innerWidth/2,window.innerHeight/2);
+    renderer.render(scene, planta);
+    
+    // Arriba Izquierda
+    renderer.setViewport(0,window.innerHeight/2,window.innerWidth/2,window.innerHeight/2);
+    renderer.render(scene, alzado);
+    
+    // Arriba Derecha
+    renderer.setViewport(window.innerWidth/2,window.innerHeight/2,window.innerWidth/2,window.innerHeight/2);
+    renderer.render(scene, perfil);
+    
+    //Abajo derecha
+    renderer.setViewport(window.innerWidth/2,0,window.innerWidth/2,window.innerHeight/2);
     renderer.render(scene, camera);
+    
 }
 function update() {
     angulo += 0.01;
